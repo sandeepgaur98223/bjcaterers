5cac83c997b4b93a90348dcd00f17e3e3584b5df
Sandeep Gaur <sandeepgaur98223@gmail.com>
2024-05-12 19:37:07 +0530

    without style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,30 +15,71 @@ export class AppComponent {
   title = 'bjcaterers';
   sum=0;
   TotalCost=0;
+  noOfPeople=0;
+  hideAddItem=true;
+  newItem="";
+  newItemPrice=0;
+  editIndex=-1;
 
-  menuitems=[{"item":"Gulab Jamun", "price": 20}
-  ,{"item":"Kachori", "price": 12}
-  ,{"item":"Samosa", "price": 15}]
+
+  menuitems=[{"item":"Gulab Jamun", "price": 20,include:false}
+  ,{"item":"Kachori", "price": 12,include:false}
+  ,{"item":"Samosa", "price": 15,include:false}]
 
   order= [];
 
-  checkBoxClicked(eventtarget:any,item:any){
+  checkBoxClicked(eventtarget:any,item:any,i:any){
       console.log("Checkbox checked:",eventtarget.checked);
 
       if(eventtarget.checked)
       {
         this.sum+=item.price;
+        this.menuitems[i].include=true;
       }
       else
       {
         this.sum-=item.price;
+        this.menuitems[i].include=false;
       }
+      this.TotalCost=this.sum*this.noOfPeople;
   }
 
   calculateTotal(noOfPeople:any)
   {
-    console.log("Number Entered:",noOfPeople);
+    this.noOfPeople=noOfPeople.value;
+    console.log("Number Entered:",this.noOfPeople);
 
-    this.TotalCost=this.sum*noOfPeople.value;
+    this.TotalCost=this.sum*this.noOfPeople;
+  }
+
+  addNewItem(){
+     let newobj= {item: this.newItem,price:this.newItemPrice,include:false};
+     this.menuitems.push(newobj);
+     this.hideAddItem=true;
+     this.newItem="";
+     this.newItemPrice=0;
+  }
+  deleteItem(i:number){
+    if(this.menuitems[i].include)
+    {
+      this.sum-=this.menuitems[i].price;
+    }
+    this.menuitems.splice(i,1);
+  }
+
+  editItem(i:number){
+    if(this.menuitems[i].include)
+    {
+      this.sum-=this.menuitems[i].price;
+    }
+    this.editIndex=i;
+
+  }
+  SaveEditedItem(i:number){
+    this.editIndex=-1;
+     if(this.menuitems[i].include)
+    {
+      this.sum+=this.menuitems[i].price;
+    }
   }
 }
