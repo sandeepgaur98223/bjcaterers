d2dbda8bf8558c44f2fdfbf4f095b8d35c99c560
Sandeep Gaur <sandeepgaur98223@gmail.com>
2024-05-12 17:22:42 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bjcaterers';
+  sum=0;
+  TotalCost=0;
+
+  menuitems=[{"item":"Gulab Jamun", "price": 20}
+  ,{"item":"Kachori", "price": 12}
+  ,{"item":"Samosa", "price": 15}]
+
+  order= [];
+
+  checkBoxClicked(eventtarget:any,item:any){
+      console.log("Checkbox checked:",eventtarget.checked);
+
+      if(eventtarget.checked)
+      {
+        this.sum+=item.price;
+      }
+      else
+      {
+        this.sum-=item.price;
+      }
+  }
+
+  calculateTotal(noOfPeople:any)
+  {
+    console.log("Number Entered:",noOfPeople);
+
+    this.TotalCost=this.sum*noOfPeople.value;
+  }
 }
